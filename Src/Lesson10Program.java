ecc2c6b53838294d7c2e0cc05a3d0be2b5f9a57e
AndreyKhrypach <pacik78@gmail.com>
2023-06-25 11:25:19 +0300

    samll fix in method reverse

--- Src/Lesson10Program.java
+++ Src/Lesson10Program.java
@@ -136,7 +136,7 @@ public class Lesson10Program {
         return sum;
     }
     public static Integer[] reverseArray(Integer[] arr){
-        for (int i = 0; i < arr.length/2 - 1; i++) {
+        for (int i = 0; i < arr.length/2; i++) {
             int temp = arr[i];
             arr[i] = arr[arr.length -i - 1];
             arr[arr.length -i - 1] = temp;
